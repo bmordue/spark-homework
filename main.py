b5c58d50135306911eea7f0dd67359b90d4dbe74
Ben Mordue <benmordue@gmail.com>
2018-08-06 20:49:34 +0100

    Passing test

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 import unittest
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import time
+
 
 class CompareAndSwitchTestCase(unittest.TestCase):
     site = 'https://sparkenergy.co.uk/compare'
@@ -7,13 +12,28 @@ class CompareAndSwitchTestCase(unittest.TestCase):
 
     def setUp(self):
         self.browser = webdriver.Firefox()
-        self.browser.implicitly_wait(10)
+        self.browser.implicitly_wait(15)
         self.addCleanup(self.browser.quit)
 
     def testPageTitle(self):
         self.browser.get(self.site)
         self.assertIn(self.siteTitle, self.browser.title)
 
+ #       self.browser.implicitly_wait(15)
+        # close cookies notice
+#        self.browser.find_element_by_xpath("//div[1]/div/a[1]").click()
+        # self.browser.find_element_by_css_selector(".cc_btn").click()
+        # self.browser.find_element_by_xpath("//a[contains(@class,'cc_btn_accept_all')]").click()
+
+#        dismiss = self.browser.findElement();
+
+#css=a.cc_btn.cc_btn_accept_all
+
+        wait = WebDriverWait(self.browser, 30)
+        element = wait.until(EC.visibility_of_element_located((By.XPATH, "//a[@class='cc_btn cc_btn_accept_all']")))
+        element = wait.until(EC.element_to_be_clickable((By.XPATH, "//a[@class='cc_btn cc_btn_accept_all']")))
+        time.sleep(2)
+        element.click()
         # "before we start -- do you have a spark account?" --> No
         self.browser.find_element_by_xpath("//div[@id='beforeWeStart']/div/div/label[2]").click()
 #        self.browser.find_element_by_xpath("//input[@id='isCustomerN']").click()
@@ -33,14 +53,14 @@ class CompareAndSwitchTestCase(unittest.TestCase):
 
         # Click next to continue
 #        self.browser.find_element_by_xpath("//main/div[1]/ul/li[2]").click()
-        self.browser.find_element_by_xpath("//li[contains(@class, 'next') and contains(@class, 'button')]").click()
-
+        self.browser.find_element_by_xpath("//li[contains(@class, 'next') and contains(@class, 'button')]/span").click()
+        # self.browser.find_element_by_xpath("//div[@id='wizard-p-1']/div[2]/div/div[3]/label[2]").click()
 
 
         # "do you know your current supplier?" --> No
 
         self.browser.find_element_by_xpath("//div[@id='wizard-p-1']/div[2]/div/div[3]/label[2]").click()
-
+#        self.browser.find_element_by_xpath("//div[@id='dontKnowCurrentSupplierRow']/div/div[2]/div/div[3]/ul/li[4]").click()
         # choose number of bedrooms --> 3 (drop-down)
 #        self.browser.find_element_by_xpath("//div[@id='dontKnowCurrentSupplierRow']/div/div[2]/div/div[3]/ul/li[4]").click()
         self.browser.find_element_by_xpath("//div[contains(@class, 'selectricWrapper')]").click()
@@ -50,14 +70,16 @@ class CompareAndSwitchTestCase(unittest.TestCase):
         # economy 7 meter installed? --> no (default selected)
 
         # click next to continue
-        self.browser.find_element_by_xpath("//main/div/ul/li[2]").clickAt(210, 45)
+        self.browser.find_element_by_xpath("//main/div/ul/li[2]").click()
         #self.browser.find_element_by_xpath("//li[contains(@class, 'next') and contains(@class, 'button')]").click()
 
 
         ### VERIFICATION
-        tariffs = self.browser.find_elements_by_xpath("//div[@id=type-1]/div[@class=tariff-block]")
-        assert len(tariffs) is 5
-        print "Passed"
+#        tariffs = self.browser.find_elements_by_xpath("//div[@id=type-1]/div[@class='tariff-block']")
+        tariffs = self.browser.find_elements_by_xpath("//div[@class='tariff-block']")
+#        assert len(tariffs) is 5
+        self.assertEqual(5, len(tariffs))
+        print("Passed")
 
 
 if __name__ == '__main__':
